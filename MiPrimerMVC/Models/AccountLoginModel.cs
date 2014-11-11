2a39d15ad10167e98d969cf5f12950303347cd8e
jmibarrad <jmibarra@unitec.edu>
2014-11-11 09:00:59 -0600

    DataAnnotations

--- MiPrimerMVC/Models/AccountLoginModel.cs
+++ MiPrimerMVC/Models/AccountLoginModel.cs
@@ -1,10 +1,17 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MiPrimerMVC.Models
 {
     public class AccountLoginModel
     {
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
         public string Email { get; set; }
+
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
     }
 }
